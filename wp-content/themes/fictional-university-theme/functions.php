2b3d1616477ada662fdd9cc19f288e7c0b4e089e
ahazimeh <hazimeh95@gmail.com>
2021-04-18 13:39:27 +0300

    completed section 5

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -8,6 +8,10 @@ function university_files() {
 }
 add_action('wp_enqueue_scripts','university_files');
 function university_features(){
+    register_nav_menu('headerMenuLocation', 'header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
+    
     add_theme_support('title-tag');// to enable a feature for the theme
 }
 add_action('after_setup_theme','university_features');
--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -25,14 +25,34 @@ get_header();
       }
       ?>
     
-    
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php
+    $testArray = get_pages(array(
+        'child_of' =>get_the_ID()
+    ));
+    if($the_parent || $testArray){
+        ?>
+
+        
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($the_parent); ?>"><?php echo get_the_title($the_parent) ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+          <?php
+          if($the_parent){
+              $findChildrenOf = $the_parent;
+          }
+          else{
+              $findChildrenOf = get_the_ID();
+          }
+          wp_list_pages(array(
+              'title_li' => NULL,//to remove the title
+              'child_of' => $findChildrenOf,
+              'sort_column' => 'menu_order'
+          ));
+          ?>
       </ul>
-    </div> -->
+    </div>
+    <?php
+    } ?>
 
     <div class="generic-content">
         <?php the_content(); ?>
